20ca95dc321ab1f982d474bc782e6c3aa832e0b1
hiroshi egami <doegadoega@hotmail.com>
2025-05-20 23:14:49 +0900

    the first commit

---
+++ remoteDesctop/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  remoteDesctop
+//
+//  Created by Hiroshi Egami on 2025/05/20.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
